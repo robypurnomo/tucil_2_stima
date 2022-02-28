56fe305b0cb2ead3fc3e04826e3a1b3fb32009c3
Roby Purnomo <74169230+robypurnomo@users.noreply.github.com>
2022-02-28 20:26:52 +0700

    final final update

--- src/lib/digits.py
+++ src/lib/digits.py
@@ -1,3 +1,4 @@
+from random import choice
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from sklearn import datasets 
@@ -10,18 +11,30 @@ def digits() :
     # create a DataFrame 
     df = pd.DataFrame(data.data, columns=data.feature_names) 
     df['Target'] = pd.DataFrame(data.target) 
-    print(df.shape)
+    print("Ukuran data :", df.shape)
+    print()
     df.head()
+
+    print("Pilih 2 data yang akan digunakan! (angka)")
+    nomor = 1
+    for i in data.feature_names :
+        print(str(nomor) + ".", i)
+        nomor += 1
+
+    print()
+    choice_x = int(input("Data pertama >> "))
+    choice_y = int(input("Data kedua >> "))
+
 
     # visualisasi ConvexHull
     plt.figure(figsize = (10, 6))
     colors = ['b','r','g']
-    plt.title('pixel_0_3 vs pixel_0_4')
-    plt.xlabel(data.feature_names[0])
-    plt.ylabel(data.feature_names[1])
+    plt.title('Convex Hull Result')
+    plt.xlabel(data.feature_names[choice_x-1])
+    plt.ylabel(data.feature_names[choice_y-1])
     for i in range(len(data.target_names)):
         bucket = df[df['Target'] == i]
-        bucket = bucket.iloc[:,[2,3]].values
+        bucket = bucket.iloc[:,[choice_x-1,choice_y-1]].values
         x, y = myConvexHull(bucket)
         plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
         plt.plot(x, y, colors[i%2])
--- src/lib/iris.py
+++ src/lib/iris.py
@@ -10,18 +10,29 @@ def iris() :
     # create a DataFrame 
     df = pd.DataFrame(data.data, columns=data.feature_names) 
     df['Target'] = pd.DataFrame(data.target) 
-    print(df.shape)
+    print("Ukuran data :", df.shape)
+    print()
     df.head()
+
+    print("Pilih 2 data yang akan digunakan! (angka)")
+    nomor = 1
+    for i in data.feature_names :
+        print(str(nomor) + ".", i)
+        nomor += 1
+
+    print()
+    choice_x = int(input("Data pertama >> "))
+    choice_y = int(input("Data kedua >> "))
 
     # visualisasi ConvexHull
     plt.figure(figsize = (10, 6))
     colors = ['b','r','g']
-    plt.title('petal Width vs petal Length')
-    plt.xlabel(data.feature_names[0])
-    plt.ylabel(data.feature_names[1])
+    plt.title('Convex Hull Result')
+    plt.xlabel(data.feature_names[choice_x-1])
+    plt.ylabel(data.feature_names[choice_y-1])
     for i in range(len(data.target_names)):
         bucket = df[df['Target'] == i]
-        bucket = bucket.iloc[:,[0,1]].values
+        bucket = bucket.iloc[:,[choice_x-1,choice_y-1]].values
         x, y = myConvexHull(bucket)
         plt.scatter(bucket[:, 0], bucket[:, 1], label=data.target_names[i])
         plt.plot(x, y, colors[i])
